ff70870eb6f068c92edc7d95d14e9f105119dea3
Timothy Lee <timothylhy@hotmail.com>
2024-09-07 22:02:25 +0800

    Added Search bar and Toast component

---
+++ frontend/src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { Search } from "lucide-react";
+import React from "react";
+import { Input } from "./ui/input";
+
+type SearchBarProps = {
+  searchTerm: string;
+  setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function SearchBar({
+  searchTerm,
+  setSearchTerm,
+}: SearchBarProps) {
+  return (
+    <form className="flex-shrink-0">
+      <div className="relative">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search Counterparty..."
+          value={searchTerm}
+          className="pl-8 w-full sm:w-[300px] md:w-[200px] lg:w-[300px]"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+}

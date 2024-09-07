6062a71c6f7716712ad6c8d52ea4bc4fa2da1420
Timothy Lee <timothylhy@hotmail.com>
2024-09-07 12:18:32 +0800

    Auth page and navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,17 +2,49 @@
 
 import React from "react";
 import { ModeToggle } from "./ModeToggle";
+import Link from "next/link";
+import { Package2 } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="h-16 border rounded w-full flex justify-between items-center px-8">
-      <div />
-      <div>
-        <h1 className="text-2xl font-bold">GIC Code to Impact</h1>
-      </div>
-      <div className="flex gap-x-4">
-        <ModeToggle />
-      </div>
+    <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
+      <Link
+        href="#"
+        className="flex items-center gap-2 text-lg font-semibold md:text-base"
+      >
+        <Package2 className="h-6 w-6" />
+        <span className="sr-only">Acme Inc</span>
+      </Link>
+      <Link
+        href="#"
+        className="text-foreground transition-colors hover:text-foreground"
+      >
+        Dashboard
+      </Link>
+      <Link
+        href="#"
+        className="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Orders
+      </Link>
+      <Link
+        href="#"
+        className="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Products
+      </Link>
+      <Link
+        href="#"
+        className="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Customers
+      </Link>
+      <Link
+        href="#"
+        className="text-muted-foreground transition-colors hover:text-foreground"
+      >
+        Analytics
+      </Link>
     </nav>
   );
 }
